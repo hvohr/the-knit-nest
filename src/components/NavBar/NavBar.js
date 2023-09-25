5127a0670215e519c4e7437ad3dc24800f0772be
Hollis Vohr <hollis.vohr@gmail.com>
2023-09-25 12:43:39 -0600

    feat: add home section base styling and cat logo

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,21 +3,24 @@ import './NavBar.css'
 
 function NavBar() {
   return (
-    <nav>
-      <section className='icon-container'>
-        <NavLink to='/'><img className='site-logo' src={require('../images/Salford sheep (1).jpeg')}></img></NavLink>
-      </section>
-      <section className='topics-container'>
-        <NavLink className='nav-topics' to='yarn'>Yarn</NavLink>
-        <NavLink className='nav-topics' to='yarn'>Craft Tools</NavLink>
-        <NavLink className='nav-topics' to='books'>Books</NavLink>
-      </section>
-      <section className='user-container'>
-        <button className='search-icon'><img className='nav-icon' src={require('../images/loupe.png')} /></button>
-        <NavLink to='login'><img className='nav-icon' src={require('../images/user (1).png')}/></NavLink>
-        <NavLink to='login'><img className='nav-icon' src={require('../images/shopping-cart.png')}/></NavLink>
-      </section>
-    </nav>
+    <section>
+      <p className='discount'>Take 20% off all Lion Brand Yarn! Use Code: Fall23</p>
+      <nav>
+        <section className='icon-container'>
+          <NavLink to='/'><img className='site-logo' src={require('../images/Salford sheep (1).jpeg')}></img></NavLink>
+        </section>
+        <section className='topics-container'>
+          <NavLink className='nav-topics' to='yarn'>Yarn</NavLink>
+          <NavLink className='nav-topics' to='crafttools'>Craft Tools</NavLink>
+          <NavLink className='nav-topics' to='books'>Books</NavLink>
+        </section>
+        <section className='user-container'>
+          <button className='search-icon'><img className='nav-icon' src={require('../images/loupe.png')} /></button>
+          <NavLink to='login'><img className='nav-icon' src={require('../images/user (1).png')} /></NavLink>
+          <NavLink to='login'><img className='nav-icon' src={require('../images/shopping-cart.png')} /></NavLink>
+        </section>
+      </nav>
+    </section>
   )
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,11 @@
+import './pages.css'
 
 function Home() {
   return (
-    <h1>Home</h1>
+    <section className='home-section'>
+      <h1 className='home-greeting'>Welcome to the Knit Nest</h1>
+      <img className='home-cat-image' src={require('../components/images/C8CA53A8-2415-440F-90C7-CFCEE5F54D0E_1_201_a.jpeg')} alt='black cat playing with a partial unstrung ball of yellow yarn'/>
+    </section>
   )
 }
 
