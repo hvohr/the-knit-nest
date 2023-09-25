8158dc909e44f04514507dbe8fa56f03c79de587
Hollis Vohr <hollis.vohr@gmail.com>
2023-09-25 14:54:38 -0600

    feat: add details to induvidual book page

--- src/pages/InduvidualBook.js
+++ src/pages/InduvidualBook.js
@@ -1,7 +1,30 @@
+import { useParams } from 'react-router-dom'
 
-function InduvidualBook() {
+function InduvidualBook(props) {
+  const { id } = useParams();
+  const singleCheck = () => {
+    return props.books.find((book) => book.id === parseInt(id))
+  }
   return (
-    <h1>Induvidual Book</h1>
+    <section>
+      <div>
+        <img src={singleCheck().image} />
+      </div>
+      <div>
+        <h1>{singleCheck().name}</h1>
+        <h3>{singleCheck().author}</h3>
+        <h4>{singleCheck().price}</h4>
+        <p>{singleCheck().description}</p>
+      </div>
+      <div>
+        <h1>{singleCheck().details.type}</h1>
+        <h3>{singleCheck().details.isbn}</h3>
+        <h4>{singleCheck().details.publisher}</h4>
+        <p>{singleCheck().details.publicationDate}</p>
+        <p>{singleCheck().details.pages}</p>
+        <p>{singleCheck().details.productDimensions}</p>
+      </div>
+    </section>
   )
 }
 
