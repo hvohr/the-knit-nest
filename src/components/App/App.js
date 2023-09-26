ad54805de03f7f76c1a1cd938490762feb1d9470
Hollis Vohr <hollis.vohr@gmail.com>
2023-09-26 12:10:35 -0600

    feat: add new route and page for specific yarn and tools

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,6 +7,8 @@ import CraftTools from '../../pages/CraftTools'
 import InduvidualItem from '../../pages/InduvidualBook'
 import Login from '../../pages/Login'
 import InduvidualBook from '../../pages/InduvidualBook'
+import InduvidualYarn from '../../pages/InduvidualYarn'
+import InduvidualTool from '../../pages/InduvidualTool'
 import Cart from '../../pages/Cart'
 import NavBar from '../NavBar/NavBar'
 import { useState, useEffect } from 'react'
@@ -35,6 +37,8 @@ function App() {
         <Route path='/cart' element={<Cart />} />
         <Route path='/login' element={<Login />} />
         <Route path='/books/:id' element={<InduvidualBook />} />
+        <Route path='/yarn/:id' element={<InduvidualYarn />} />
+        <Route path='/crafttools/:id' element={<InduvidualTool />} />
       </Routes>
     </div>
   );
