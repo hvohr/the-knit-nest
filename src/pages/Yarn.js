a336a928077389787a977fd6d24ab9256badcab2
Hollis Vohr <hollis.vohr@gmail.com>
2023-10-02 16:56:51 -0600

    feat: add display for all refine sections

--- src/pages/Yarn.js
+++ src/pages/Yarn.js
@@ -6,17 +6,10 @@ import { getSpecificProduct } from '../components/apiCalls'
 function Yarn() {
   const [yarn, setYarn] = useState([])
   const [allRefine, setAllRefine] = useState([])
-  const [refineColor, setRefineColor] = useState([])
-  const [refineBrand, setRefineBrand] = useState([])
-  const [refineWeight, setRefineWeight] = useState([])
-
   const [showColor, setShowColor] = useState(false)
   const [showBrand, setShowBrand] = useState(false)
   const [showYarnWeight, setShowYarnWeight] = useState(false)
 
-  function checkBoxChange() {
-
-  }
 
   function getSpecific() {
     getSpecificProduct('yarn').then(
@@ -31,9 +24,8 @@ function Yarn() {
     console.log(allRefine)
   }, [allRefine])
 
-  function handleColorChecked(event) {
+  function handleChecked(event) {
     if (event.target.checked) {
-      setRefineColor([...refineColor, event.target.name])
       setAllRefine([...allRefine, event.target])
     } else {
       let newDisplay = allRefine.filter((ref) => ref !== event.target)
@@ -43,7 +35,7 @@ function Yarn() {
 
   let refineDisplay = allRefine.map((ref) => {
     return (
-      <h3>{ref.name}: {ref.id}</h3>
+      <h4>{ref.value}: {ref.name}</h4>
     )
   })
 
@@ -55,42 +47,42 @@ function Yarn() {
         <button className='refine-button' onClick={() => setShowColor(!showColor)}>↓ Color</button>
         {showColor && <div>
           <div className='price-container'>
-            <input type='checkbox' onChange={(event) => handleColorChecked(event)} id='red' name='color' />
+            <input type='checkbox' value='color' onChange={(event) => handleChecked(event)} id='red' name='Red' />
             <span className="dot-red"></span>
             <label htmlFor="red">Red</label>
           </div>
           <div className='price-container'>
-            <input type='checkbox' id='green' onChange={(event) => handleColorChecked(event)} name='color' />
+            <input type='checkbox' value='color' id='green' onChange={(event) => handleChecked(event)} name='Green' />
             <span className="dot-green"></span>
             <label htmlFor="green">Green</label>
           </div>
           <div className='price-container'>
-            <input type='checkbox' id='blue' onChange={(event) => handleColorChecked(event)} name='color' />
+            <input type='checkbox' value='color' id='blue' onChange={(event) => handleChecked(event)} name='Blue' />
             <span className="dot-blue"></span>
             <label htmlFor="blue">Blue</label>
           </div>
           <div className='price-container'>
-            <input type='checkbox' id='yellow' onChange={(event) => handleColorChecked(event)} name='color' />
+            <input type='checkbox' value='color' id='yellow' onChange={(event) => handleChecked(event)} name='Yellow' />
             <span className="dot-yellow"></span>
             <label htmlFor="yellow">Yellow</label>
           </div>
           <div className='price-container'>
-            <input type='checkbox' id='purple' onChange={(event) => handleColorChecked(event)} name='color' />
+            <input type='checkbox' value='color' id='purple' onChange={(event) => handleChecked(event)} name='Purple' />
             <span className="dot-purple"></span>
             <label htmlFor="purple">Purple</label>
           </div>
           <div className='price-container'>
-            <input type='checkbox' id='orange' onChange={(event) => handleColorChecked(event)} name='color' />
+            <input type='checkbox' value='color' id='orange' onChange={(event) => handleChecked(event)} name='Orange' />
             <span className="dot-orange"></span>
             <label htmlFor="orange">Orange</label>
           </div>
           <div className='price-container'>
-            <input type='checkbox' id='white' onChange={(event) => handleColorChecked(event)} name='color' />
+            <input type='checkbox' value='color' id='white' onChange={(event) => handleChecked(event)} name='White' />
             <span className="dot-white"></span>
             <label htmlFor="white">White</label>
           </div>
           <div className='price-container'>
-            <input type='checkbox' id='black' onChange={(event) => handleColorChecked(event)} name='color' />
+            <input type='checkbox' value='color' id='black' onChange={(event) => handleChecked(event)} name='Black' />
             <span className="dot-black"></span>
             <label htmlFor="black">Black</label>
           </div>
@@ -98,50 +90,50 @@ function Yarn() {
         <button className='refine-button' onClick={() => setShowBrand(!showBrand)}>↓ Brand</button>
         {showBrand && <div>
           <div className='price-container'>
-            <input type='checkbox' id='lion' name='brand' />
+            <input type='checkbox' id='lion-brand' value='brand' onChange={(event) => handleChecked(event)} name='Lion Brand' />
             <label htmlFor="lion">Lion Brand</label>
           </div>
           <div className='price-container'>
-            <input type='checkbox' id='bernat' name='brand' />
+            <input type='checkbox' id='bernat' value='brand' onChange={(event) => handleChecked(event)} name='Bernat' />
             <label htmlFor="bernat">Bernat</label>
           </div>
           <div className='price-container'>
-            <input type='checkbox' id='lily' name='brand' />
+            <input type='checkbox' id='lily' value='brand' onChange={(event) => handleChecked(event)} name='Lily Sugar/n Cream' />
             <label htmlFor="lily">Lily Sugar'n Cream</label>
           </div>
           <div className='price-container'>
-            <input type='checkbox' id='red-heart' name='brand' />
+            <input type='checkbox' id='red-heart' value='brand' onChange={(event) => handleChecked(event)} name='Red Heart' />
             <label htmlFor="red-heart">Red Heart</label>
           </div>
         </div>}
         <button className='refine-button' onClick={() => setShowYarnWeight(!showYarnWeight)}>↓ Yarn Weight</button>
         {showYarnWeight && <div>
           <div className='price-container'>
-            <input type='checkbox' id='lace' name='lace' />
+            <input type='checkbox' id='lace' value='Yarn Weight' onChange={(event) => handleChecked(event)} name='Lace' />
             <label htmlFor="lace">0 - Lace</label>
           </div>
           <div className='price-container'>
-            <input type='checkbox' id='super' name='super' />
+            <input type='checkbox' id='super' value='Yarn Weight' onChange={(event) => handleChecked(event)} name='Super Fine' />
             <label htmlFor="super">1 - Super Fine</label>
           </div>
           <div className='price-container'>
-            <input type='checkbox' id='fine' name='fine' />
+            <input type='checkbox' id='fine' value='Yarn Weight' onChange={(event) => handleChecked(event)} name='Fine' />
             <label htmlFor="fine">2 - Fine</label>
           </div>
           <div className='price-container'>
-            <input type='checkbox' id='light' name='light' />
+            <input type='checkbox' id='light' value='Yarn Weight' onChange={(event) => handleChecked(event)} name='Light' />
             <label htmlFor="light">3- Light</label>
           </div>
           <div className='price-container'>
-            <input type='checkbox' id='medium' name='medium' />
+            <input type='checkbox' id='medium' value='Yarn Weight' onChange={(event) => handleChecked(event)} name='Medium' />
             <label htmlFor="medium">4 - Medium</label>
           </div>
           <div className='price-container'>
-            <input type='checkbox' id='bulky' name='bulky' />
+            <input type='checkbox' id='bulky' value='Yarn Weight' onChange={(event) => handleChecked(event)} name='Bulky' />
             <label htmlFor="bulky">5 - Bulky</label>
           </div>
           <div className='price-container'>
-            <input type='checkbox' id='super-bulky' name='super-bulky' />
+            <input type='checkbox' id='super-bulky' value='Yarn Weight' onChange={(event) => handleChecked(event)} name='Super-Bulky' />
             <label htmlFor="super-bulky">6 - Super Bulky</label>
           </div>
         </div>}
