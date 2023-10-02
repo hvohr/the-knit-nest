b614402868991b3b92b38bc18eb34c27b1cf0f72
Hollis Vohr <hollis.vohr@gmail.com>
2023-10-02 16:47:54 -0600

    feat: add refine display for user on yarn page

--- src/pages/Yarn.js
+++ src/pages/Yarn.js
@@ -5,11 +5,18 @@ import { getSpecificProduct } from '../components/apiCalls'
 
 function Yarn() {
   const [yarn, setYarn] = useState([])
-  const [refine, setRefine] = useState([])
+  const [allRefine, setAllRefine] = useState([])
+  const [refineColor, setRefineColor] = useState([])
+  const [refineBrand, setRefineBrand] = useState([])
+  const [refineWeight, setRefineWeight] = useState([])
+
   const [showColor, setShowColor] = useState(false)
   const [showBrand, setShowBrand] = useState(false)
   const [showYarnWeight, setShowYarnWeight] = useState(false)
 
+  function checkBoxChange() {
+
+  }
 
   function getSpecific() {
     getSpecificProduct('yarn').then(
@@ -21,7 +28,24 @@ function Yarn() {
 
   useEffect(() => {
     getSpecific()
-  }, [])
+    console.log(allRefine)
+  }, [allRefine])
+
+  function handleColorChecked(event) {
+    if (event.target.checked) {
+      setRefineColor([...refineColor, event.target.name])
+      setAllRefine([...allRefine, event.target])
+    } else {
+      let newDisplay = allRefine.filter((ref) => ref !== event.target)
+      setAllRefine(newDisplay)
+    }
+  }
+
+  let refineDisplay = allRefine.map((ref) => {
+    return (
+      <h3>{ref.name}: {ref.id}</h3>
+    )
+  })
 
 
   return (
@@ -31,100 +55,102 @@ function Yarn() {
         <button className='refine-button' onClick={() => setShowColor(!showColor)}>↓ Color</button>
         {showColor && <div>
           <div className='price-container'>
-            <input type='checkbox' id='red' name='red' />
+            <input type='checkbox' onChange={(event) => handleColorChecked(event)} id='red' name='color' />
             <span className="dot-red"></span>
-            <label for="red">Red</label>
+            <label htmlFor="red">Red</label>
           </div>
           <div className='price-container'>
-            <input type='checkbox' id='green' name='green' />
+            <input type='checkbox' id='green' onChange={(event) => handleColorChecked(event)} name='color' />
             <span className="dot-green"></span>
-            <label for="green">Green</label>
+            <label htmlFor="green">Green</label>
           </div>
           <div className='price-container'>
-            <input type='checkbox' id='blue' name='blue' />
+            <input type='checkbox' id='blue' onChange={(event) => handleColorChecked(event)} name='color' />
             <span className="dot-blue"></span>
-            <label for="blue">Blue</label>
+            <label htmlFor="blue">Blue</label>
           </div>
           <div className='price-container'>
-            <input type='checkbox' id='yellow' name='yellow' />
+            <input type='checkbox' id='yellow' onChange={(event) => handleColorChecked(event)} name='color' />
             <span className="dot-yellow"></span>
-            <label for="yellow">Yellow</label>
+            <label htmlFor="yellow">Yellow</label>
           </div>
           <div className='price-container'>
-            <input type='checkbox' id='purple' name='purple' />
+            <input type='checkbox' id='purple' onChange={(event) => handleColorChecked(event)} name='color' />
             <span className="dot-purple"></span>
-            <label for="purple">Purple</label>
+            <label htmlFor="purple">Purple</label>
           </div>
           <div className='price-container'>
-            <input type='checkbox' id='orange' name='orange' />
+            <input type='checkbox' id='orange' onChange={(event) => handleColorChecked(event)} name='color' />
             <span className="dot-orange"></span>
-            <label for="orange">Orange</label>
+            <label htmlFor="orange">Orange</label>
           </div>
           <div className='price-container'>
-            <input type='checkbox' id='white' name='white' />
+            <input type='checkbox' id='white' onChange={(event) => handleColorChecked(event)} name='color' />
             <span className="dot-white"></span>
-            <label for="white">White</label>
+            <label htmlFor="white">White</label>
           </div>
           <div className='price-container'>
-            <input type='checkbox' id='black' name='black' />
+            <input type='checkbox' id='black' onChange={(event) => handleColorChecked(event)} name='color' />
             <span className="dot-black"></span>
-            <label for="black">Black</label>
+            <label htmlFor="black">Black</label>
           </div>
         </div>}
         <button className='refine-button' onClick={() => setShowBrand(!showBrand)}>↓ Brand</button>
         {showBrand && <div>
           <div className='price-container'>
-            <input type='checkbox' id='lion' name='lion' />
-            <label for="lion">Lion Brand</label>
+            <input type='checkbox' id='lion' name='brand' />
+            <label htmlFor="lion">Lion Brand</label>
           </div>
           <div className='price-container'>
-            <input type='checkbox' id='bernat' name='bernat' />
-            <label for="bernat">Bernat</label>
+            <input type='checkbox' id='bernat' name='brand' />
+            <label htmlFor="bernat">Bernat</label>
           </div>
           <div className='price-container'>
-            <input type='checkbox' id='lily' name='lily' />
-            <label for="lily">Lily Sugar'n Cream</label>
+            <input type='checkbox' id='lily' name='brand' />
+            <label htmlFor="lily">Lily Sugar'n Cream</label>
           </div>
           <div className='price-container'>
-            <input type='checkbox' id='red-heart' name='red-heart' />
-            <label for="red-heart">Red Heart</label>
+            <input type='checkbox' id='red-heart' name='brand' />
+            <label htmlFor="red-heart">Red Heart</label>
           </div>
         </div>}
         <button className='refine-button' onClick={() => setShowYarnWeight(!showYarnWeight)}>↓ Yarn Weight</button>
         {showYarnWeight && <div>
           <div className='price-container'>
             <input type='checkbox' id='lace' name='lace' />
-            <label for="lace">0 - Lace</label>
+            <label htmlFor="lace">0 - Lace</label>
           </div>
           <div className='price-container'>
             <input type='checkbox' id='super' name='super' />
-            <label for="super">1 - Super Fine</label>
+            <label htmlFor="super">1 - Super Fine</label>
           </div>
           <div className='price-container'>
             <input type='checkbox' id='fine' name='fine' />
-            <label for="fine">2 - Fine</label>
+            <label htmlFor="fine">2 - Fine</label>
           </div>
           <div className='price-container'>
             <input type='checkbox' id='light' name='light' />
-            <label for="light">3- Light</label>
+            <label htmlFor="light">3- Light</label>
           </div>
           <div className='price-container'>
             <input type='checkbox' id='medium' name='medium' />
-            <label for="medium">4 - Medium</label>
+            <label htmlFor="medium">4 - Medium</label>
           </div>
           <div className='price-container'>
             <input type='checkbox' id='bulky' name='bulky' />
-            <label for="bulky">5 - Bulky</label>
+            <label htmlFor="bulky">5 - Bulky</label>
           </div>
           <div className='price-container'>
             <input type='checkbox' id='super-bulky' name='super-bulky' />
-            <label for="super-bulky">6 - Super Bulky</label>
+            <label htmlFor="super-bulky">6 - Super Bulky</label>
           </div>
         </div>}
       </section>
       <section className='small-yarn-container'>
         <h1 className='page-title'>Yarn</h1>
         <p className='page-description'>Tailored especially for beginners eager to embark on their crafting journey. Our selection boasts user-friendly textures and shades, ideal for those new to knitting or crocheting. Dive in to discover beginner-friendly yarns that ensure your first projects are both enjoyable and successful.</p>
+        {allRefine.length !== 0 && refineDisplay}
+        {allRefine.length === 0 && <p>Viewing all {yarn.length} products</p>}
         <div>
           {yarn.length && <SingleProduct products={yarn} />}
         </div>
