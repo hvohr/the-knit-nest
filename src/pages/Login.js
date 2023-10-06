93248a8c90bae492e5dd2af6731a93cbf47d53ab
Hollis Vohr <hollis.vohr@gmail.com>
2023-10-06 16:27:29 -0600

    feat: add login page and styling

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,26 @@
+import { Link } from 'react-router-dom'
 
 function Login() {
   return (
-    <h1>Login</h1>
+    <section className='login-container'>
+      <div className='login-top'>
+        <h1 className='login-welcome'>Welcome Back to The Knit Nest<img className='login-kitten' src={require('../components/images/cat (1).png')} /></h1>
+      </div>
+      <form>
+        <div className='email-container'>
+          <label>Email<span className='required'>*</span></label>
+          <input type='email' name='login-email' className='login-email' />
+        </div>
+        <div className='password-container'>
+          <label>Password<span className='required'>*</span></label>
+          <input type='email' name='login-password' className='login-email' />
+        </div>
+        <div>
+          <button className='login-submit'>Log in</button>
+          <h2 className='login-create'>Not a member with us? <Link to='/createaccount'>Create an account</Link></h2>
+        </div>
+      </form>
+    </section>
   )
 }
 
