b88a9efe5fcc39ad398f41556a3b73f38369c20b
Hollis Vohr <hollis.vohr@gmail.com>
2023-10-11 13:03:42 -0600

    feat: add product length

--- src/pages/Books.js
+++ src/pages/Books.js
@@ -16,6 +16,10 @@ function Books() {
     )
   }
 
+  function sortBooks() {
+
+  }
+
   useEffect(() => {
     getSpecific()
     console.log(filter)
@@ -25,30 +29,33 @@ function Books() {
     <section>
       <h1 className='page-title'>Books</h1>
       <p className='page-description'>Discover a world of knowledge and imagination in our diverse collection of crafting books. Whether you're an avid reader, a curious learner, or searching for the perfect crocheting/knitting project, our carefully curated selection offers something for everyone!</p>
-      <div className='filtering-container'>
-        <button className='filter-button' onClick={() => setClose(!close)}>Sort by</button>
-        {!close && <div className='filter-items'>
-          <button className='filter-button1' onClick={(event) => {
-            setFilter(event.target.value)
-            setClose(true)
-          }} value='Price High to Low'>Price High to Low</button>
-          <button className='filter-button1' onClick={(event) => {
-            setFilter(event.target.value)
-            setClose(true)
-          }} value='Price Low to High'>Price Low to High</button>
-          <button className='filter-button1' onClick={(event) => {
-            setFilter(event.target.value)
-            setClose(true)
-          }} value='A-Z'>A-Z</button>
-          <button className='filter-button1' onClick={(event) => {
-            setFilter(event.target.value)
-            setClose(true)
-          }} value='A-Z'>Z-A</button>
-        </div>}
-        {filter && <div className='close-container'>
-          <button className='close-button' onClick={() => setFilter('')}><img className='close-filter' src={require('../components/images/close (2).png')} /></button>
-          <p className='ui-sort'>Sorting by: {filter}</p>
-        </div>}
+      <div className='top-container'>
+        <p className='product-length'>Viewing all {books.length} product(s)</p>
+        <div className='filtering-container'>
+          <button className='filter-button' onClick={() => setClose(!close)}>Sort by</button>
+          {!close && <div className='filter-items'>
+            <button className='filter-button1' onClick={(event) => {
+              setFilter(event.target.value)
+              setClose(true)
+            }} value='Price High to Low'>Price High to Low</button>
+            <button className='filter-button1' onClick={(event) => {
+              setFilter(event.target.value)
+              setClose(true)
+            }} value='Price Low to High'>Price Low to High</button>
+            <button className='filter-button1' onClick={(event) => {
+              setFilter(event.target.value)
+              setClose(true)
+            }} value='A-Z'>A-Z</button>
+            <button className='filter-button1' onClick={(event) => {
+              setFilter(event.target.value)
+              setClose(true)
+            }} value='A-Z'>Z-A</button>
+          </div>}
+          {filter && <div className='close-container'>
+            <button className='close-button' onClick={() => setFilter('')}><img className='close-filter' src={require('../components/images/close (2).png')} /></button>
+            <p className='ui-sort'>Sorting by: {filter}</p>
+          </div>}
+        </div>
       </div>
       <div>
         {books.length && <SingleProduct products={books} />}
