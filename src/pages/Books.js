3c017c77ce3bf2c04661bcdb2971719295388b38
Hollis Vohr <hollis.vohr@gmail.com>
2023-10-11 12:49:56 -0600

    feat: add ui and styling for filter

--- src/pages/Books.js
+++ src/pages/Books.js
@@ -26,25 +26,28 @@ function Books() {
       <h1 className='page-title'>Books</h1>
       <p className='page-description'>Discover a world of knowledge and imagination in our diverse collection of crafting books. Whether you're an avid reader, a curious learner, or searching for the perfect crocheting/knitting project, our carefully curated selection offers something for everyone!</p>
       <div className='filtering-container'>
-        <button onClick={() => setClose(!close)}>Sort by</button>
-        {filter && <p>Sorting by: {filter}</p>}
-        {!close && <div>
-          <button className='filter' onClick={(event) => {
+        <button className='filter-button' onClick={() => setClose(!close)}>Sort by</button>
+        {!close && <div className='filter-items'>
+          <button className='filter-button1' onClick={(event) => {
             setFilter(event.target.value)
             setClose(true)
           }} value='Price High to Low'>Price High to Low</button>
-          <button className='filter' onClick={(event) => {
+          <button className='filter-button1' onClick={(event) => {
             setFilter(event.target.value)
             setClose(true)
           }} value='Price Low to High'>Price Low to High</button>
-          <button className='filter' onClick={(event) => {
+          <button className='filter-button1' onClick={(event) => {
             setFilter(event.target.value)
             setClose(true)
           }} value='A-Z'>A-Z</button>
-          <button className='filter' onClick={(event) => {
+          <button className='filter-button1' onClick={(event) => {
             setFilter(event.target.value)
             setClose(true)
           }} value='A-Z'>Z-A</button>
+        </div>}
+        {filter && <div className='close-container'>
+          <button className='close-button' onClick={() => setFilter('')}><img className='close-filter' src={require('../components/images/close (2).png')} /></button>
+          <p className='ui-sort'>Sorting by: {filter}</p>
         </div>}
       </div>
       <div>
