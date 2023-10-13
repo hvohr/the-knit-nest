58f75883bb9548ccbbc7f5f2a05996120938a822
Hollis Vohr <hollis.vohr@gmail.com>
2023-10-12 18:04:52 -0600

    feat: make account info front responsive

--- src/pages/AccountInfo.js
+++ src/pages/AccountInfo.js
@@ -32,7 +32,7 @@ function AccountInfo() {
 
   if (!currentUser) {
     return (
-      <section className='account-info-container'>
+      <section className='account-info-container2'>
         <h1>Welcome User!</h1>
         <img className='login-kitten' src={require('../components/images/cat-with-ball-of-yarn-by-Vexels.png')} />
         <h2><Link className='login-link' to='/login'>Login</Link> to view account information or <Link className='create-account-link' to='/createaccount'>create an account</Link> with us if you are new to the Knit Nest!</h2>
